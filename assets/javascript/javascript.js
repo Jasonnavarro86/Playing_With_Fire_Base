dd4beef443f06c2e2767f9cedc7f6502f34d7761
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-03 13:57:53 -0400

    JSON Array OBJ.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,21 +1,22 @@
 localStorage.setItem("name", "Jason");
 localStorage.setItem("country", "USA");
-console.log(localStorage);
+// console.log(localStorage);
 // localStorage.removeItem("name");
 // localStorage.clear();
-console.log(localStorage.length);
-console.log(localStorage.key(2));
+// console.log(localStorage.length);
+// console.log(localStorage.key(2));
 
 $("<h1>").addClass("container").html("Name: " + localStorage.name).appendTo("body")
 $("<h1>").addClass("container").html("Country: " + localStorage.country).appendTo("body")
 
 $("#clickMe").on("click", function () {
     event.preventDefault();
-     localStorage.setItem("age", $("#theval").val());
+    localStorage.setItem("age", $("#theval").val());
     $("<h1>").addClass("container").html("Age: " + localStorage.age).appendTo("body")
     $('#theval').val("")
 })
 
-var newObjSyn = '{"firstname" : "jason", "lastname":"navarro"}'
-var newTest = JSON.parse(newObjSyn);
-console.log(newTest.firstname);
+var newObjSyn = '[{"firstname" : "jason", "lastname":"navarro"}, {"firstname" : "jess", "lastname":"navarro"}]';
+var test1 = JSON.parse(newObjSyn);
+console.log(test1[0].firstname);
+console.log(test1[1].firstname);
