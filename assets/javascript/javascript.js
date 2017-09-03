e6d5177d2ebfd3c27104b8100a4d21c122681dd3
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-03 12:36:46 -0400

    Set the localStorage with and input val

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,28 @@
 localStorage.setItem("name", "Jason");
 localStorage.setItem("country", "USA");
-localStorage.setItem("Age", "31");
-$("<h1>").addClass("container").html(localStorage.name).appendTo("body")
-$("<h1>").addClass("container").html(localStorage.country).appendTo("body")
-$("<h1>").addClass("container").html(localStorage.Age).appendTo("body")
+
+
+
+
+console.log(localStorage);
+
+// localStorage.removeItem("name");
+
+// localStorage.clear();
+
+console.log(localStorage.length);
+
+console.log(localStorage.key(2));
+
+$("<h1>").addClass("container").html("Name: " + localStorage.name).appendTo("body")
+$("<h1>").addClass("container").html("Country: " + localStorage.country).appendTo("body")
+
+
+$("#clickMe").on("click", function(){
+    event.preventDefault();
+
+    localStorage.setItem("age",$("#theval").val());
+    $("<h1>").addClass("container").html("Age: " + localStorage.age).appendTo("body")
+    $('#theval').val("")
+})
+
