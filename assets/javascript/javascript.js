e03e09912dff826ddaa6ac1455150129c83355fd
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-03 13:36:59 -0400

    Found a way to make a JSON ready for localStorage.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,28 +1,21 @@
 localStorage.setItem("name", "Jason");
 localStorage.setItem("country", "USA");
-
-
-
-
 console.log(localStorage);
-
 // localStorage.removeItem("name");
-
 // localStorage.clear();
-
 console.log(localStorage.length);
-
 console.log(localStorage.key(2));
 
 $("<h1>").addClass("container").html("Name: " + localStorage.name).appendTo("body")
 $("<h1>").addClass("container").html("Country: " + localStorage.country).appendTo("body")
 
-
-$("#clickMe").on("click", function(){
+$("#clickMe").on("click", function () {
     event.preventDefault();
-
-    localStorage.setItem("age",$("#theval").val());
+     localStorage.setItem("age", $("#theval").val());
     $("<h1>").addClass("container").html("Age: " + localStorage.age).appendTo("body")
     $('#theval').val("")
 })
 
+var newObjSyn = '{"firstname" : "jason", "lastname":"navarro"}'
+var newTest = JSON.parse(newObjSyn);
+console.log(newTest.firstname);
